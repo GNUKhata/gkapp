f6e84032252eae75212cd406a1769513ec8bf7f2
Kannan V M <kannan@disroot.org>
2024-09-30 01:07:13 +0530

    feat: update sidebar to handle updated accounts module URL

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -143,7 +143,7 @@
               >
                 <b-icon icon="server" /><translate> Audit Logs</translate>
               </b-nav-item>
-              <b-nav-item to="/accounts/-1/-1/-1">
+              <b-nav-item to="/accounts">
                 <b-icon icon="files-alt" /><translate> Accounts</translate>
               </b-nav-item>
               <b-nav-item to="/budgets/cash/-1">
