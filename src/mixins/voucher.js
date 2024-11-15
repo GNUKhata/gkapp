b9bff4d9663ff29a5dbdf79ca066d4dd0f508e18
Bady <bady@disroot.org>
2024-11-15 10:05:35 +0530

    fix: API calls to /accountsbyrule API endpoint throws 404 due to invalid params

--- src/mixins/voucher.js
+++ src/mixins/voucher.js
@@ -416,7 +416,6 @@ export default {
       this.addRow('cr');
       this.addRow('dr');
       this.updateDate();
-      this.preloadData(true);
       return this.updateAccounts(skipVtype, !!refreshFlag);
     },
     updateDate() {
